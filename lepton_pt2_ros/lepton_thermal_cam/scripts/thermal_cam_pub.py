d063e98c356c0f2a4677f2854ab7c467ca64bff6
Wenderson Machado Ferreira <wenderson.ferreira@sicoob.com.br>
2023-07-29 09:23:06 -0300

    Ajustes para gravacao video

--- lepton_pt2_ros/lepton_thermal_cam/scripts/thermal_cam_pub.py
+++ lepton_pt2_ros/lepton_thermal_cam/scripts/thermal_cam_pub.py
@@ -9,9 +9,57 @@ import rospy # Python library for ROS
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
+from uvctypes import * # Types UVC library
+import time # Time library
+import numpy as np # Numpy library
+
+# Queue
+try:
+  from queue import Queue
+except ImportError:
+  from Queue import Queue
+import platform
+
+BUF_SIZE = 2
+q = Queue(BUF_SIZE)
+
+def py_frame_callback(frame, userptr):
+
+  array_pointer = cast(frame.contents.data, POINTER(c_uint16 * (frame.contents.width * frame.contents.height)))
+  data = np.frombuffer(
+    array_pointer.contents, dtype=np.dtype(np.uint16)
+  ).reshape(
+    frame.contents.height, frame.contents.width
+  ) # no copy
+
+  if frame.contents.data_bytes != (2 * frame.contents.width * frame.contents.height):
+    return
+
+  if not q.full():
+    q.put(data)
+
+PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(py_frame_callback)
+
+def ktof(val):
+  return (1.8 * ktoc(val) + 32.0)
+
+def ktoc(val):
+  return (val - 273) / 100.0
   
+def raw_to_8bit(data):
+  cv2.normalize(data, data, 0, 65535, cv2.NORM_MINMAX)
+  np.right_shift(data, 8, data)
+  return cv2.cvtColor(np.uint8(data), cv2.COLOR_GRAY2RGB)
+
+def display_temperature(img, val_k, loc, color):
+  val = ktoc(val_k)
+  cv2.putText(img,"{0:.1f} degC".format(val), loc, cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+  x, y = loc
+  cv2.line(img, (x - 2, y), (x + 2, y), color, 1)
+  cv2.line(img, (x, y - 2), (x, y + 2), color, 1)
+    
 def publish_message():
- 
+
   # Node is publishing to the video_frames topic using 
   # the message type Image
   pub = rospy.Publisher('thermal_frames', Image, queue_size=10)
@@ -24,37 +72,92 @@ def publish_message():
   # Go through the loop 10 times per second
   rate = rospy.Rate(10) # 10hz
      
-  # Create a VideoCapture object
-  # The argument '0' gets the default webcam.
-  cap = cv2.VideoCapture(0)
+  # Create a LibUVC object for capture radiometric video
+  ctx = POINTER(uvc_context)()
+  dev = POINTER(uvc_device)()
+  devh = POINTER(uvc_device_handle)()
+  ctrl = uvc_stream_ctrl()
 
-  if not cap.isOpened():
-    print ("Camera not found!")
+  # Initialize LibUVC object
+  res = libuvc.uvc_init(byref(ctx), 0)
+  if res < 0:
+    print("uvc_init error")
     exit(1)
+    
+  try:
+    # Get device camera object
+    res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
+    if res < 0:
+      print("uvc_find_device error")
+      exit(1)
+
+    try:
+      # Open device camera object
+      res = libuvc.uvc_open(dev, byref(devh))
+      if res < 0:
+        print("uvc_open error")
+        exit(1)
+
+      print("device opened!")
+
+      print_device_info(devh)
+      print_device_formats(devh)
+      
+      # Get frame camera format
+      frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+      if len(frame_formats) == 0:
+        print("device does not support Y16")
+        exit(1)
+
+      libuvc.uvc_get_stream_ctrl_format_size(devh, byref(ctrl), UVC_FRAME_FORMAT_Y16,
+        frame_formats[0].wWidth, frame_formats[0].wHeight, int(1e7 / frame_formats[0].dwDefaultFrameInterval)
+      )
+      
+      # Start stream camera
+      res = libuvc.uvc_start_streaming(devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0)
+      if res < 0:
+        print("uvc_start_streaming failed: {0}".format(res))
+        exit(1)
+
+      try:
+        # Used to convert between ROS and OpenCV images
+        br = CvBridge()
      
-  # Used to convert between ROS and OpenCV images
-  br = CvBridge()
- 
-  # While ROS is still running.
-  while not rospy.is_shutdown():
-     
-      # Capture frame-by-frame
-      # This method returns True/False as well
-      # as the video frame.
-      ret, frame = cap.read()
+        # While ROS is still running.
+        while not rospy.is_shutdown():
          
-      if ret == True:
-        # Print debugging information to the terminal
-        rospy.loginfo('publishing thermal frame')
-             
-        # Publish the image.
-        # The 'cv2_to_imgmsg' method converts an OpenCV
-        # image to a ROS image message
-        pub.publish(br.cv2_to_imgmsg(frame))
-             
-      # Sleep just enough to maintain the desired rate
-      rate.sleep()
-         
+          # Capture frame-by-frame
+          # This method returns True/False as well
+          # as the video frame.
+          data = q.get(True, 500)
+          if data is None:
+            break
+          data = cv2.resize(data[:,:], (640, 480))
+          minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(data)
+          img = raw_to_8bit(data)
+          display_temperature(img, minVal, minLoc, (255, 0, 0))
+          display_temperature(img, maxVal, maxLoc, (0, 0, 255))
+          
+          # Print debugging information to the terminal
+          rospy.loginfo('publishing thermal frame')
+                 
+          # Publish the image.
+          # The 'cv2_to_imgmsg' method converts an OpenCV
+          # image to a ROS image message
+          pub.publish(br.cv2_to_imgmsg(frame))
+          
+          # Sleep just enough to maintain the desired rate
+          rate.sleep()
+                 
+        #cv2.destroyAllWindows()
+      finally:
+        libuvc.uvc_stop_streaming(devh)
+      print("done")
+    finally:
+      libuvc.uvc_unref_device(dev)
+  finally:
+    libuvc.uvc_exit(ctx)
+    
 if __name__ == '__main__':
   try:
     publish_message()
--- lepton_pt2_ros/lepton_thermal_cam/scripts/thermal_cam_sub.py
+++ lepton_pt2_ros/lepton_thermal_cam/scripts/thermal_cam_sub.py
@@ -10,7 +10,8 @@ import rospy # Python library for ROS
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
- 
+import time # Time library
+
 def callback(data):
  
   # Used to convert between ROS and OpenCV images
@@ -22,20 +23,30 @@ def callback(data):
   # Convert ROS Image message to OpenCV image
   current_frame = br.imgmsg_to_cv2(data)
   
-  # Display image
-  cv2.imshow("camera", cv2.resize(current_frame, (640, 480)))
+  # so, convert them from float to integer.
+  frame_width = 80
+  frame_height = 60
+   
+  file_size = (frame_width, frame_height)
   
-  filename = 'thermal_video.mp4'
-  file_size = (640, 480) # Assumes 640x480 mp4
-  frames_per_second = 20.0
+  hms = time.strftime('%H:%M:%S', time.localtime())
+  
+  file_path = '~/videos/thermal_' + str(hms) + '.avi'
+  
+  frames_per_second = 20
  
-  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-  result = cv2.VideoWriter(filename,  
+  # Display image
+  #cv2.imshow("camera", cv2.resize(current_frame, (640, 480)))
+ 
+  fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+  video = cv2.VideoWriter(file_path,  
                            fourcc, 
                            frames_per_second, 
                            file_size)
+                           
+  self.video = video
   
-  result.write(current_frame)
+  self.video.write(current_frame)
   
   cv2.waitKey(1)
       
@@ -52,8 +63,10 @@ def receive_message():
   # spin() simply keeps python from exiting until this node is stopped
   rospy.spin()
  
+  self.video.release()
+  
   # Close down the video stream when done
-  cv2.destroyAllWindows()
+  #cv2.destroyAllWindows()
   
 if __name__ == '__main__':
   receive_message()
